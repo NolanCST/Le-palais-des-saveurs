2f36d7af781b518431749e8a80912cf306a979e2
Zaïnab <146081803+ZainabZM@users.noreply.github.com>
2023-09-27 09:25:48 +0200

    Update ajoutProduits.js

--- ajoutProduits.js
+++ ajoutProduits.js
@@ -1,27 +1,6 @@
 let formProduit = document.querySelectorAll(".formulaireProduit");
 let AddBtn = document.querySelector("#AddBtn");
 let container = document.querySelector(".mainCont");
-let subCont = document.querySelector(".subCont");
 
-let LSProductArray = JSON.parse(localStorage.getItem("@produitArray"));
-
-let productArray;
-
-if (LSProductArray) {
-  productArray = LSProductArray;
-  renderContact(productArray);
-} else {
-  productArray = [];
-}
-
-function ContactPro(Produit, firstName, email, phone, siret, companyName) {
-  this.lastName = lastName;
-  this.firstName = firstName;
-  this.email = email;
-  this.phone = phone;
-  this.siret = siret;
-  this.companyName = companyName;
-  this.type = "pro";
-}
 
 AddBtn.addEventListener("click", renderProduct);
