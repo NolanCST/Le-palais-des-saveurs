b894bde333bfb171f453a84173d762bfaad62d2a
NolanCST <nolan.costa06@gmail.com>
2023-09-27 20:33:01 +0100

    Activation du bouton pour supprimer les produits du stock

--- produits.js
+++ produits.js
@@ -1,20 +1,36 @@
-let container = document.querySelector(".container");
+// FONCTIONS
+
+function renderStock(array) {
+  contProduct.innerHTML = "";
+  array.forEach(function (element, index) {
+    contProduct.innerHTML += `<span>${index + 1} | ${element.nameProduct}</span><input class="quantityStock" type="number"/>
+        <span>${element.type} | ${element.purchasingPriceHT}€ | ${element.sellingPriceHT}€ | ${element.tva} | ${element.margeHT}€ | ${element.sellingPriceTTC}€ | ${element.degree}°</span><button class="btnDel">X</button><br>`;
+  });
+  deleteProduct();
+}
+
+function deleteProduct() {
+  let btnDelArray = document.querySelectorAll(".btnDel");
+  btnDelArray.forEach(function (element, index) {
+    element.addEventListener("click", function () {
+      if (confirm("Voulez-vous supprimer le produit ?")) {
+        renderStockArray.splice(index, 1);
+        localStorage.setItem("@StockArray", JSON.stringify(renderStockArray));
+        renderStock(renderStockArray);
+      }
+    });
+  });
+}
+
+// LANCEMENT DE LA PAGE
+
+let contProduct = document.querySelector(".contProduct");
 const recupLS = localStorage.getItem("@StockArray");
 let renderStockArray;
-
 if (recupLS) {
   renderStockArray = JSON.parse(recupLS);
-  renderSTock(renderStockArray);
+  renderStock(renderStockArray);
 } else {
   renderStockArray = [];
 }
-
-function renderSTock(array) {
-  array.forEach(function (element, index) {
-    container.innerHTML += `<div class="">
-        <span>${index + 1} | ${element.nameProduct}</span>
-        <input class="quntityStock" type="number"/>
-        <span>${element.type} | ${element.purchasingPriceHT}€ | ${element.sellingPriceHT}€ | ${element.tva} | ${element.margeHT}€ | ${element.sellingPriceTTC}€ | ${element.degree}°</span>
-        </div>`;
-  });
-}
+console.log(renderStockArray);
