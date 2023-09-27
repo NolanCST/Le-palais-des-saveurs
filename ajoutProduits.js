134155e9d1f8bd1d0a3fd00eb1099d6977ee26f9
NolanCST <nolan.costa06@gmail.com>
2023-09-27 10:06:10 +0200

    Debut des fonctionnalites js ajoutProduits

--- ajoutProduits.js
+++ ajoutProduits.js
@@ -1,27 +1,121 @@
-let formProduit = document.querySelectorAll(".formulaireProduit");
-let AddBtn = document.querySelector("#AddBtn");
-let container = document.querySelector(".mainCont");
-let subCont = document.querySelector(".subCont");
+// VARIABLES
+let product = document.querySelector(".product");
+let quantity = document.querySelector(".quantity");
+let purchasingPriceHT = document.querySelector(".purchasingPriceHT");
+let sellingPriceHT = document.querySelector(".sellingPriceHT");
+let margeHT = document.querySelector(".margeHT");
+let sellingPriceTTC = document.querySelector(".sellingPriceTTC");
+let type = document.querySelector(".type");
+let degree = document.querySelector(".degree");
+let TVA = document.querySelector(".TVA");
+const stockArray = [];
 
-let LSProductArray = JSON.parse(localStorage.getItem("@produitArray"));
-
-let productArray;
-
-if (LSProductArray) {
-  productArray = LSProductArray;
-  renderContact(productArray);
-} else {
-  productArray = [];
+// Prix toutes taxes comprises
+function calculPriceTTC() {
+  let calculTTC;
+  if (type.value == "ba") {
+    TVA = 0.2;
+    calculTTC = sellingPriceHT * (1 + TVA);
+  } else {
+    TVA = 0.055;
+    calculTTC = sellingPriceHT * (1 + TVA);
+  }
+  sellingPriceTTC.innerText = calculTTC;
+  console.log("PENIS");
 }
 
-function ContactPro(Produit, firstName, email, phone, siret, companyName) {
-  this.lastName = lastName;
-  this.firstName = firstName;
-  this.email = email;
-  this.phone = phone;
-  this.siret = siret;
-  this.companyName = companyName;
-  this.type = "pro";
-}
+calculPriceTTC();
 
-AddBtn.addEventListener("click", renderProduct);
+// // marge hors taxes
+// function calculMargeHT() {
+//   return prixVenteHT - prixAchatHT;
+// }
+
+// function ajouterProduit(stock, produit) {
+//   stock.push(produit);
+// }
+
+// let boissonAlcoolisée = new BoissonAlcoolisee(
+//   nom,
+//   quantite,
+//   prixAchatHT,
+//   prixDeVenteHT,
+//   margeHT,
+//   prixDeVenteTTC,
+//   degreAlcool
+// );
+// let boissonNonAlcoolisée = new BoissonNonAlcoolisee(
+//   nom,
+//   quantite,
+//   prixAchatHT,
+//   prixDeVenteHT,
+//   margeHT,
+//   prixDeVenteTTC
+// );
+// let autreProduit = new AutreProduit(
+//   nom,
+//   quantite,
+//   prixAchatHT,
+//   prixDeVenteHT,
+//   margeHT,
+//   prixDeVenteTTC
+// );
+
+// ajouterProduit(stock, boissonAlcoolisee);
+// ajouterProduit(stock, boissonNonAlcoolisee);
+// ajouterProduit(stock, autreProduit);
+
+// afficherStock(stock);
+
+// function BoissonAlcoolisee(
+//   nom,
+//   quantite,
+//   prixAchatHT,
+//   prixDeVenteHT,
+//   margeHT,
+//   prixDeVenteTTC,
+//   degreAlcool
+// ) {
+//   this.nom = nom;
+//   this.quantite = quantite;
+//   this.prixAchatHT = prixAchatHT;
+//   this.prixDeVenteHT = prixDeVenteHT;
+//   this.margeHT = margeHT;
+//   this.prixDeVenteTTC = prixDeVenteTTC;
+//   this.degreAlcool = degreAlcool;
+//   this.type = "ba";
+// }
+
+// function BoissonNonAlcoolisee(
+//   nom,
+//   quantite,
+//   prixAchatHT,
+//   prixDeVenteHT,
+//   margeHT,
+//   prixDeVenteTTC
+// ) {
+//   this.nom = nom;
+//   this.quantite = quantite;
+//   this.prixAchatHT = prixAchatHT;
+//   this.prixDeVenteHT = prixDeVenteHT;
+//   this.margeHT = margeHT;
+//   this.prixDeVenteTTC = prixDeVenteTTC;
+//   this.type = "bna";
+// }
+
+// function AutreProduit(
+//   nom,
+//   quantite,
+//   prixAchatHT,
+//   prixDeVenteHT,
+//   margeHT,
+//   prixDeVenteTTC
+// ) {
+//   this.nom = nom;
+//   this.quantite = quantite;
+//   this.prixAchatHT = prixAchatHT;
+//   this.prixDeVenteHT = prixDeVenteHT;
+//   this.margeHT = margeHT;
+//   this.prixDeVenteTTC = prixDeVenteTTC;
+//   this.type = "autre";
+// }
