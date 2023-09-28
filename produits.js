c9df04a7be1fcf0ef59e0053e94eec8c30dda944
NolanCST <nolan.costa06@gmail.com>
2023-09-28 10:00:27 +0200

    Modif background input stock

--- produits.js
+++ produits.js
@@ -1,12 +1,23 @@
 // FONCTIONS
-
 function renderStock(array) {
   contProduct.innerHTML = "";
   array.forEach(function (element, index) {
-    contProduct.innerHTML += `<span>${index + 1} | Nom: ${element.nameProduct} | Quantite: </span><input class="quantityStock" type="number" value="${element.quantity}"/>
+    const backgroundColor = element.quantity >= 10 ? "#01D758" : "#DC143C"; // Condition de couleur de fond initiale
+    contProduct.innerHTML += `<span>${index + 1} | Nom: ${element.nameProduct} | Quantite: </span><input class="quantityStock" type="number" value="${element.quantity}" style="background-color: ${backgroundColor}" />
         <span>Type: ${element.type} | Prix d'achat HT: ${element.purchasingPriceHT}€ | Prix de vente HT: ${element.sellingPriceHT}€ | TVA: ${element.tva} | Marge HT: ${element.margeHT}€ | Prix de vente TTC: ${element.sellingPriceTTC}€ | Degre: ${
       element.degree
     }°</span><button class="btnDel">X</button><br>`;
+    let quantityStock = document.querySelector(".quantityStock");
+    quantityStock.addEventListener("change", function () {
+      if (quantityStock.value >= 10) {
+        quantityStock.style.backgroundColor = "#01D758";
+      } else {
+        quantityStock.style.backgroundColor = "#DC143C";
+      }
+      const newQuantity = parseInt(quantityStock.value); // Obtenez la nouvelle quantité depuis l'input
+      element.quantity = newQuantity; // Mettez à jour la quantité dans le tableau
+      localStorage.setItem("@StockArray", JSON.stringify(renderStockArray)); // Mettez à jour le localStorage
+    });
   });
   deleteProduct();
 }
